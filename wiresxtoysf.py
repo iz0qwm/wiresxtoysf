2fc05f9d6583ccc58d466d83d0bc176b2ec65d64
iz0qwm <RaDa0707>
2018-08-12 21:41:26 +0200

    First commit

---
+++ wiresxtoysf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Author: Raffaello Di Martino IZ0QWM
+# Date: 12.08.2018
+# Version 0.1
+
+import aprslib
+import logging
+import time
+import sched
+from time import sleep
+from datetime import datetime
+import base64
+import math
+import threading
+from threading import Timer
+import thread
+import re
+import sys
+import configparser
+import os
+import requests
+import socket
+import string
+
+UDP_IP = "kwos.dydns.org"
+UDP_PORT = 5005
+MESSAGE = "Hello, World!"
+
+print "UDP target IP:", UDP_IP
+print "UDP target port:", UDP_PORT
+print "message:", MESSAGE
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
